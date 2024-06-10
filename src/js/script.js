fc5d8310dc5ce69ccf97e60b01f1acbc5d88107b
Peu06 <jpcaetano2006@gmail.com>
2024-06-10 09:06:04 -0300

    Melhoria no codigo

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
-$(document).ready(function(){
-    $('#mobile_btn').on('click', function () {
-        $('#mobile_area').toggleClass('active');
-        $('#mobile_btn').find('i').toggleClass('fa-x');
+document.addEventListener("DOMContentLoaded", () => {
+    const mobileBtn = document.getElementById("mobile_btn");
+    const mobileArea = document.getElementById("mobile_area");
+
+    mobileBtn.addEventListener("click", () => {
+        mobileArea.classList.toggle("navbar__mobile-area--active");
+        mobileBtn.querySelector('i').classList.toggle('fa-x');
     });
 });
